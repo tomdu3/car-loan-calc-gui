83af3be1819a628055d1781c7f9a7294a6e1171f
Tomislav Dukez <tomdu3@ymail.com>
2023-12-22 21:57:37 +0000

    Add test label widgets

--- run.py
+++ run.py
@@ -4,6 +4,12 @@ window = tk.Tk()
 window.title("Car Loan Calculator")
 window.geometry("800x500")
 
+my_label = tk.Label(window, text='Hello world', fg='white', bg='black', font=('Helvetica', 32), width=100)
+my_label.pack()
+
+my_label2 = tk.Label(window, text='Second thing')
+my_label2.pack(pady=20)
+
 window.mainloop()
 
 

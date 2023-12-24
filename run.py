2c9e5a63a133d675debc7dd02d2f657dbfbc69a4
Tomislav Dukez <tomdu3@ymail.com>
2023-12-24 19:03:48 +0000

    Add: try grid

--- run.py
+++ run.py
@@ -4,13 +4,16 @@ window = tk.Tk()
 window.title("Car Loan Calculator")
 window.geometry("800x500")
 
-my_label = tk.Label(window, text='Hello world', fg='white', bg='black', font=('Helvetica', 32), width=100)
-my_label.pack()
+my_label = tk.Label(
+    window,
+    text="Hello world",
+    fg="white",
+    bg="black",
+    font=("Times New Roman", 32),
+)
+my_label.grid(row=0, column=0)
 
-my_label2 = tk.Label(window, text='Second thing')
-my_label2.pack(pady=20)
+my_label2 = tk.Label(window, text="Second thing")
+my_label2.grid(row=1, column=1)
 
 window.mainloop()
-
-
-

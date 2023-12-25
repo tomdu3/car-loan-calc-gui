7ceb03a32ea5b6583c83b0273b14a9fce04357ce
Tomislav Dukez <tomdu3@ymail.com>
2023-12-25 14:31:58 +0000

    Add button

--- run.py
+++ run.py
@@ -1,5 +1,12 @@
 import tkinter as tk
 
+
+# Create clicked function
+def clicked(name):
+    my_label2 = tk.Label(window, text=name)
+    my_label2.grid(row=1, column=0)
+
+
 window = tk.Tk()
 window.title("Car Loan Calculator")
 window.geometry("800x500")
@@ -14,6 +21,10 @@ my_label = tk.Label(
 my_label.grid(row=0, column=0)
 
 my_label2 = tk.Label(window, text="Second thing")
-my_label2.grid(row=1, column=1)
+my_label2.grid(row=1, column=0)
+
+# create buttons
+my_button = tk.Button(window, text='Click me!', command=lambda: clicked('You clicked me!'))
+my_button.grid(row=1, column=1)
 
 window.mainloop()
